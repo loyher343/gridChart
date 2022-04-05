47997f6b8067d576cc120283349dfd92abad980d
loyher343 <loyhernandez59@gmail.com>
2022-04-05 00:10:48 -0500

    chart setup needs realtime data

--- gridbot.js
+++ gridbot.js
@@ -7,6 +7,41 @@ console.log(socket)
 
 const quotesElement = document.getElementById('quotes')
 const tradesElement = document.getElementById('trades')
+
+var chart = LightweightCharts.createChart(document.body, {
+	width: 600,
+  height: 300,
+	crosshair: {
+		mode: LightweightCharts.CrosshairMode.Normal,
+	},
+    layout: {
+        backgroundColor: '#000000',
+        textColor: '#ffffff',
+    },
+    grid: {
+        vertLines: {
+            color: '#404040',
+        },
+        horzLines: {
+            color: '#404040',
+        },
+    },
+    priceScale: {
+        borderColor: '#cccccc',
+    },
+    timeScale: {
+        borderColor: '#cccccc',
+        timeVisible: true,
+    }
+});
+
+var candleSeries = chart.addCandlestickSeries();
+var data = [
+	{ time: '2018-10-19', open: 54.62, high: 55.50, low: 54.52, close: 54.90 },
+	{ time: '2018-10-22', open: 55.08, high: 55.27, low: 54.61, close: 54.98 },
+	{ time: '2018-10-23', open: 56.09, high: 57.47, low: 56.09, close: 57.21 },
+	{ time: '2018-10-24', open: 57.00, high: 58.44, low: 56.41, close: 57.42 }]
+candleSeries.setData(data);
 socket.onmessage = function(event) {
     const data = JSON.parse(event.data);
     const message = data[0]['msg']
@@ -61,3 +96,4 @@ socket.onmessage = function(event) {
         }
     }
 }
+//958'
